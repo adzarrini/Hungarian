1749c7cde1eb686a6c27f36681fb623824498121
jgreshik <jgreshik@mymail.mines.edu>
2019-12-10 23:14:45 -0700

    added do_serial option to run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -2,12 +2,13 @@ make
 matrix_dir=matrix
 data_dir=data
 out_temp=out_temp.txt
-out_file=${1} # out.txt
+out_file=${2} # out.txt
+do_serial=${1}
 if [ "$out_file" == '' ]
 then
     out_file=out.txt
 fi
-all_tests=(10 100 500 1000 2000 3000 4000 5000) #10000 15000)
+all_tests=(10 100 500 1000) #2000 3000 4000 5000 7500 10000 12500 15000 17500 20000)
 if [ ! -d "./${data_dir}" ]
 then
     echo Creating directory ./${data_dir}/
@@ -18,7 +19,7 @@ then
     echo Creating directory ./${matrix_dir}/
     mkdir ${matrix_dir}
 fi
-printf "n\t\tcpu_out\t\tgpu_out\t\tcpu_time\t\tgpu_time\n" >> ${data_dir}/${out_temp}
+printf "n\t\tgpu_out\t\tgpu_time\t\tcpu_out\t\tcpu_time\n" >> ${data_dir}/${out_temp}
 for i in "${all_tests[@]}"
 do
     name=test_${i}.txt
@@ -28,11 +29,15 @@ do
         echo Creating matrix ./${matrix_dir}/$name
         ./creatrix $i 9 1234 ${name}
     fi
-    cpu_out=$(./hungarian_serial ./${matrix_dir}/${name} max 0)
+    cpu_out=''
+    if [ "$do_serial" == 1 ]
+    then
+        cpu_out=$(./hungarian_serial ./${matrix_dir}/${name} max 0)
+    fi
     gpu_out=$(./hungarian_parallel ./${matrix_dir}/${name} max 0)
     parse_cpu=($cpu_out)
     parse_gpu=($gpu_out)
-    line="${parse_cpu[0]}\t\t${parse_cpu[1]}\t\t${parse_gpu[1]}\t\t${parse_cpu[2]}\t\t${parse_gpu[2]}\n"
+    line="${parse_gpu[0]}\t\t${parse_gpu[1]}\t\t${parse_gpu[2]}\t\t${parse_cpu[1]}\t\t${parse_cpu[2]}\n"
     printf $line >> ${data_dir}/${out_temp}
 done
 column -t ${data_dir}/${out_temp} > ${data_dir}/${out_file}
